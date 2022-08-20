e93097c19bcc58edfc94c6d31896560c2f42ff39
Mayank <code.mayank20@gmail.com>
2022-08-20 18:33:06 +0530

    added commemnts for readability

--- integer_to_roman.java
+++ integer_to_roman.java
@@ -2,20 +2,20 @@ import java.util.*;
 public class integer_to_roman {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
-        int num= scn.nextInt();
-       int[] value={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-       String[] roman= {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int num= scn.nextInt();   //Taking input
+       int[] value={1000,900,500,400,100,90,50,40,10,9,5,4,1};   //creating a array in decreasing order for loop
+       String[] roman= {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};      // creating a similar array for the corresponding roman values
 
-       StringBuilder str= new StringBuilder();
+       StringBuilder str= new StringBuilder();  // creating a stringbuilder to store the roman number
        int i=0;
-       for(i=0;i<value.length;i++){
+       for(i=0;i<value.length;i++){    //loop to assign values accordingly
 
-           while(num>=value[i]){
-               num-=value[i];
-               str.append(roman[i]);
+           while(num>=value[i]){  //another loop to decrease the number after assigning the first roman alphabet
+               num-=value[i];     //decreasing the number by the roman numeral that is being appended
+               str.append(roman[i]);  // appending the roman numeral 
             }
        }
-System.out.println(str.toString());
+System.out.println(str.toString());  //output
     }
 
 }
--- roman_number.java
+++ roman_number.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 
 class roman_number{
-    int value(char r){
+    int value(char r){ //creating a function to return the integers corresponding to the roman numerals
         if(r=='I')
         return 1;
         if(r=='V')
@@ -23,31 +23,31 @@ class roman_number{
         return 500;
         return -1;
     }
-int convert(String x){
-    int i, total=0;
-    for(i=0;i<x.length();i++){
-        int c1=value(x.charAt(i));
-        if(i+1<x.length()){
-        int c2=value(x.charAt(i+1));
-        if(c1<c2){
-            total-=c1;
+int convert(String x){   // a function to convert the roman numeral
+    int i, total=0;          //total:to operate on the number in the loop
+    for(i=0;i<x.length();i++){   //loop to ron the length of the numeral
+        int c1=value(x.charAt(i));  // taking each character of the numeral and getting its value from the function
+        if(i+1<x.length()){    //ensuring its in the limit
+        int c2=value(x.charAt(i+1)); //taking the next characters value in the numeral
+        if(c1<c2){ // condition for IV, IX and so on
+            total-=c1;  //decreasing the number if true
 
         }
         else
-        total+=c1;
+        total+=c1;  // adding the value of each roman numeral one by one to get the answer
     }
     else
-    total+=c1;
+    total+=c1;    //if there is no IV, IX sort of roman numeral then adding the corresponding value to get the answer
     }
-    return total;
+    return total;  // output
 }
 public static void main(String[] args)
 {
 roman_number object= new roman_number();
-    Scanner scn = new Scanner(System.in);
+    Scanner scn = new Scanner(System.in); //input
     String rom= scn.next();
-    rom= rom.toUpperCase();
-    int ans= object.convert(rom);
-    System.out.println(ans);
+    rom= rom.toUpperCase(); //converting to uppercase 
+    int ans= object.convert(rom);  // calling the function
+    System.out.println(ans);  //output
 }
 }
