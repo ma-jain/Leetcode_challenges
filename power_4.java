ab4beca6d8d1bdeb69138773c38d4de27f88d42d
Mayank <code.mayank20@gmail.com>
2022-08-24 11:31:36 +0530

    Raw code

---
+++ power_4.java
@@ -0,0 +1,12 @@
+public class power_4 {
+    public static void main(String[] args){
+        int n=16;
+        if (n%4!=0||n==0)
+        System.out.println("not true");
+    double a= (Math.log(n))/(Math.log(4));
+    if (Math.floor(a)==Math.ceil(a))
+    System.out.println("true");
+    
+    }
+    
+}
